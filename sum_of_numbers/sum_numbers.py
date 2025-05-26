d0b2935630edd68ae880b67a4c6b630371c2802c
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-05-26 15:29:34 +0300

    A Python program using a for loop to calculate the sum of all the numbers in a list.

---
+++ sum_of_numbers/sum_numbers.py
@@ -0,0 +1,15 @@
+# sum_numbers.py
+# This script calculates the sum of all numbers in a list using a for loop.
+
+# Create a list of numbers
+numbers = [1, 5, 3, 9]
+
+# Store the sum of the numbers in the list
+total = 0
+
+# Iterate over the numbers in the list summing them up
+for number in numbers:
+    total += number
+
+# Print sum of all the numbers in the list
+print(f"The sum of the numbers is: {total}")

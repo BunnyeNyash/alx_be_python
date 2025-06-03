ea17ad7f57fe1a230b7c0a78bd276b1a3c0b7e35
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-03 07:44:45 +0300

    A function to calculate the area of a rectangle

---
+++ functions/area_of_rectangle.py
@@ -0,0 +1,16 @@
+"""
+Exercise 2: Create a function to calculate the area of a rectangle.
+
+Instructions:
+
+Define a function that takes two parameters, length and width.
+Inside the function, multiply the length and width to calculate the area.
+Use the return statement to return the calculated area.
+"""
+
+def area_of_rectangle(length, width):
+  area = length * width
+  return area
+
+# Usage
+print(area_of_rectangle(10, 5))  # Output: 50

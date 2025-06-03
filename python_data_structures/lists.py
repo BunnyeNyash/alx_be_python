bd0601971e069811d2b466329e09c821b6407486
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-03 09:12:51 +0300

    A list to store favorite fruits ad output second element

---
+++ python_data_structures/lists.py
@@ -0,0 +1,6 @@
+"""
+Exercise 1: Create a list to store names of your favorite fruits. Write code to access the second element and print it.
+"""
+
+fruits = ["pineapple", "apple", "cherry", "grapes"]
+print(fruits[1])      # Output: apple
